ec53f3f5e0204f4b980e94565926d8e0fa0c4d6c
Abdi Deresa <Abdishegute@gmail.com>
2024-08-26 14:58:12 -0700

    added functions and imports

---
+++ file.py
@@ -0,0 +1,39 @@
+import os
+
+from tkinter import *
+from tkinter import filedialog, colorchooser, font
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+
+
+def change_color():
+    pass
+
+
+def change_font(*args):
+    pass
+
+def new_file():
+    pass
+
+def open_file():
+    pass
+
+def save_file():
+    pass
+
+def cut():
+    pass
+
+def copy():
+    pass
+
+def paste():
+    pass
+
+def about():
+    pass
+
+def quit():
+    pass
+

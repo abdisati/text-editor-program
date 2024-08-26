2015966456424bc4e3cd23d9235515d0dc19b408
Abdi Deresa <Abdishegute@gmail.com>
2024-08-26 15:20:12 -0700

    created a text area and scroll bar

--- file.py
+++ file.py
@@ -37,3 +37,40 @@ def about():
 def quit():
     pass
 
+window = Tk()
+window.title("Text editor program")
+file = None
+
+window_width = 500
+window_height = 500
+
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2)-(window_width/2))
+y = int((screen_height/2)-(window_height/2))
+
+
+window.geometry("{}x{}+{}+{}".format(window_width, window_height,x,y))
+
+font_name = StringVar(window)
+font_name.set("Arial")
+
+font_size = StringVar(window)
+font_size.set("25")
+
+text_area = Text(window, font=(font_name.get(),font_size.get()))
+
+scroll_bar = Scrollbar(text_area)
+window.grid_rowconfigure(0,weight=1)
+window.grid_columnconfigure(0,weight=1)
+text_area.grid(sticky=N+E+S+W)
+
+
+
+
+
+scroll_bar.pack(side=RIGHT, fill=Y)
+
+
+window.mainloop()
